b3507a66c33526a4eadaac4573e2fab8d9fba14b
Alysson <alyssos@hotmail.com>
2024-11-12 23:50:06 -0300

    impresao

--- index.js
+++ index.js
@@ -10,9 +10,39 @@ app.get('/alunos', async (request,response) => {
         {nome: "Breno", idade : 27},
         {nome: "Caroline", idade : 22}
     ]
+
+    const url = 'http://localhost:3000/imprimir';
+    const dados = {
+    nome: 'Alysson',
+    messege: 'oi jarom'
+    };
+
+    enviarRequisicaoPost(url, dados);
     response.status(200).json(alunos)
 })
 
 app.listen(3001, () => {
     console.log("Runnung serve ")
 })
+
+
+
+const request = require('request');
+
+const enviarRequisicaoPost = (url, dados) => {
+    request.post(
+        {
+            url: url,
+            json: dados
+        },
+        (error, response, body) => {
+            if (error) {
+                console.error('Erro na requisição:', error);
+            } else {
+                console.log('Resposta do servidor:', body);
+            }
+        }
+    );
+};
+
+
