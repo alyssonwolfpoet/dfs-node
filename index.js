2b947d6bb53c8608717db933f6b5d5e0dfb94e8f
Alysson <alyssos@hotmail.com>
2024-11-22 11:41:38 -0300

    feat : router get post and put

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 const express = require('express')
-const app = express()
+const pg = require('pg')
+
+const app = express() 
+const { Pool } = pg
+
+const pool = new Pool({
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: 'admin123',
+    database: 'avanti'
+})
 
 app.use(express.json())
 
-app.get('/alunos', async (request,response) => {
-
-    const alunos = [
-        {nome: "Cairo", idade : 25},
-        {nome: "Breno", idade : 27},
-        {nome: "Caroline", idade : 22}
-    ]
-
-    response.status(200).json(alunos)
+app.get('/clientes', async (request,response) => {
+   const clientes = await pool.query('SELECT * FROM clientes')
+    return response.status(200).json(clientes.rows)
 })
+
+app.post('/cliente', async (request,response) => {
+    const {nome, email, telefone } = request.body
+    const clientes = await pool.query('INSERT INTO clientes(nome, email, telefone) VALUES ( $1, $2, $3)',[nome,email,telefone])
+    return response.status(201).send()
+}) 
+
+app.put('/cliente:id', async (request,response) => {
+    const {nome, email, telefone } = request.body
+    const {id} = request.params
+    const clientes = await pool.query('UPDATE clientes SET nome = $1, email = $2, telefone = $3 WHERE id = $4)',[nome,email,telefone,id])
+    return response.status(200).send()
+}) 
 
 app.listen(3001, () => {
     console.log("Runnung serve ")
